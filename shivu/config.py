8e74452096219121efb79ce2ac0d2367fb3a68a2
nan20psp <pyaesone10psp@gmail.com>
2025-08-19 09:15:14 +0630

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "1318826936"
+    sudo_users = "5821905026", "1318826936"
+    GROUP_ID = -1002356385851
+    TOKEN = "8345134806:AAGc_ygSXBH2rZz7omtpjytraXj_Yyrbr-s"
+    mongo_url = "mongodb+srv://wanglin:wanglin@wanglin.ppt93bd.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://files.catbox.moe/sygsr7.jpg", "https://files.catbox.moe/8mpep6.jpg"]
+    SUPPORT_CHAT = "sasukemusicsupportchat"
+    UPDATE_CHAT = "sasukevipmusicbotsupport"
+    BOT_USERNAME = "charactercollections_bot"
+    CHARA_CHANNEL_ID = "-1002459775779"
+    api_id = 29758428
+    api_hash = "51f9369e03f78674ca21aee8dfd1c842"
 
     
 class Production(Config):
