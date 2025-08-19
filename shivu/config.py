6983821f6ab3b3b02face5bce8e1fb039967e686
nan20psp <pyaesone10psp@gmail.com>
2025-08-19 09:41:46 +0630

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "1318826936"
     sudo_users = "5821905026", "1318826936"
-    GROUP_ID = -1002356385851
+    GROUP_ID = "-1002356385851"
     TOKEN = "8345134806:AAGc_ygSXBH2rZz7omtpjytraXj_Yyrbr-s"
     mongo_url = "mongodb+srv://wanglin:wanglin@wanglin.ppt93bd.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://files.catbox.moe/sygsr7.jpg", "https://files.catbox.moe/8mpep6.jpg"]
@@ -12,7 +12,7 @@ class Config(object):
     UPDATE_CHAT = "sasukevipmusicbotsupport"
     BOT_USERNAME = "charactercollections_bot"
     CHARA_CHANNEL_ID = "-1002459775779"
-    api_id = 29758428
+    api_id = "29758428"
     api_hash = "51f9369e03f78674ca21aee8dfd1c842"
 
     
